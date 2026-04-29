2c3a6c2c11959332d4f7e493e69d794b88568be9
Kenil Dhola <kenildhola111@gmail.com>
2026-04-29 19:13:50 +0530

    remove unnecessary file

--- aura_retail_os/aura_retail_os/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Aura Retail OS - Path A prototype package."""
-
-from .factories import PharmacyKioskFactory, FoodKioskFactory, EmergencyReliefKioskFactory
-from .simulation import run_all_scenarios
